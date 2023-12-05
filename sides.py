894a962a0165706a40e069c9b4cf4fb58798ea5b
Brogs <brogan.bennett@svu.edu>
2023-12-05 13:03:36 -0500

    added yet another function

---
+++ sides.py
@@ -0,0 +1,26 @@
+functionbrogs1(var2):
+    print("this is brogs function2")
+    return var1 / 5
+
+
+
+
+class sides(models.Model):
+    def __int__(self):
+        self.items = {}
+        self.total_price = 0
+    types = [
+        "fries"
+        "sweet potato fries"
+        "macaroni and cheese"
+        "chili"
+    ]
+    return
+
+class shakes(models.Model):
+    types = [
+        "strawberry"
+        "raspberry"
+        "chocolate"
+        "vanilla"
+    ]
